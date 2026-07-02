ab542467d5d2f1d1bbaeca95774d8ffb73437cb5
srvnkumar <v.saravana@gmail.com>
2026-07-02 13:52:28 +0530

    updated Read me

--- Builder/src/main/java/org/example/org/example/fluentbuilder/Main.java
+++ Builder/src/main/java/org/example/org/example/fluentbuilder/Main.java
@@ -1,6 +1,4 @@
-package org.example;
-
-import org.example.org.example.fluentbuilder.Car;
+package org.example.org.example.fluentbuilder;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ Builder/src/main/java/org/example/org/example/fluentbuilder/ReadMe.txt
@@ -0,0 +1,7 @@
+Pattern1 1:
+Builder Pattern :
+- A class has many fields
+- Some fields are optional
+- you want immutability of the object
+- you want readable object cration
+- you want to avoid telescoping constructor anti-pattern
